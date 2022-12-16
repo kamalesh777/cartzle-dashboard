495f688099505692b587a196967d4217fe7de356
Kamalesh Maity <ksmaity777@gmail.com>
2022-12-16 22:30:57 +0530

    layout folder moved into src folder

--- layout/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <><div>App</div><h1>Kamalesh</h1></>
-  )
-}
-
-export default App
---
+++ src/layout/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+      <h1>Kamalesh</h1>
+    </>
+  )
+}
+
+export default App
