1eb7854532e24afb9d54e56ee256ebe962b2a065
kamalesh.maity <kamalesh.maity@codeclouds.in>
2025-01-13 09:47:44 +0530

    Fix - furniture dashboard data

---
+++ src/modules/Dashboard/data.js
@@ -0,0 +1,32 @@
+export default data = [
+  {
+    title: 'Goods',
+    href: '/goods',
+    icon: 'DropboxOutlined',
+    background: '',
+  },
+  {
+    title: 'Goods',
+    href: '/goods',
+    icon: 'DropboxOutlined',
+    background: '',
+  },
+  {
+    title: 'Documents',
+    href: '/documents',
+    icon: 'FileJpgOutlined',
+    background: '',
+  },
+  {
+    title: 'Purchase',
+    href: '/purchase',
+    icon: 'AppstoreAddOutlined',
+    background: '',
+  },
+  {
+    title: 'Sell',
+    href: '/sell',
+    icon: 'AppstoreAddOutlined',
+    background: '',
+  },
+]
--- src/modules/Dashboard/index.tsx
+++ src/modules/Dashboard/index.tsx
@@ -3,18 +3,24 @@ import React from 'react'
 import { CardWrapper } from '@/components/Wrapper'
 import IconWrapper from '@/components/Wrapper/IconWrapper'
 
+import data from './data'
+
 const DashboardComponent = (): JSX.Element => {
   return (
     <div>
-      <CardWrapper>
-        <IconWrapper icon="CodeSandboxOutlined" />
-        <h2>Hello World</h2>
-      </CardWrapper>
-      <CardWrapper>
-        <IconWrapper icon="HomeOutlined" />
-        {/* {renderDynamicIcon('HomeOutlined')} */}
-        <h2>Hello Kamalesh</h2>
-      </CardWrapper>
+      {data?.map(item => (
+        <>
+          <CardWrapper key={item?.title}>
+            <IconWrapper icon="CodeSandboxOutlined" />
+            <h2>{item?.title}</h2>
+          </CardWrapper>
+          <CardWrapper>
+            <IconWrapper icon={item.icon} />
+            {/* {renderDynamicIcon('HomeOutlined')} */}
+            <h2>{item.title}</h2>
+          </CardWrapper>
+        </>
+      ))}
     </div>
   )
 }
