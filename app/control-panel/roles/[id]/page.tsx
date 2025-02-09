b942a111124a0e4a19f507736a6eb1765e9ee575
kamalesh777 <ksmaity777@gmail.com>
2025-02-09 20:14:47 +0530

    Dev - permission ui

---
+++ app/control-panel/roles/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import RoleManageComp from '@/modules/controlPanel/roles/manage'
+
+const NewRolePage = (): JSX.Element => {
+  return <RoleManageComp />
+}
+
+export default NewRolePage
--- src/modules/controlPanel/roles/index.tsx
+++ src/modules/controlPanel/roles/index.tsx
@@ -1,33 +1,34 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 
 import { SearchOutlined } from '@ant-design/icons'
 import { Input, Space } from 'antd'
+
+import { usePathname, useRouter } from 'next/navigation'
 
 import DynamicPageLayout from '@/components/DynamicPageLayout'
 import { ButtonWrapper } from '@/components/Wrapper'
 import { CONTROL_PANEL_ROUTE } from '@/constants/AppConstant'
 
-import CreateUserModaComp from './create'
-import UserSettingList from './list'
+import Rolelist from './list'
 
 const RoleSettingComp = (): JSX.Element => {
-  const [openCreateModal, setOpenCreateModal] = useState(false)
+  const router = useRouter()
+  const pathname = usePathname()
 
   // main component for render
   const mainComp = (
     <>
-      <UserSettingList />
-      {openCreateModal && <CreateUserModaComp open={openCreateModal} setOpen={setOpenCreateModal} />}
+      <Rolelist />
     </>
   )
 
   const actionComponent = (
     <Space>
       <Input prefix={<SearchOutlined />} placeholder="Search..." />
-      <ButtonWrapper type="primary" onClick={() => setOpenCreateModal(true)}>
-        Add
+      <ButtonWrapper type="primary" onClick={() => router.push(`${pathname}/new-role`)}>
+        New Role
       </ButtonWrapper>
     </Space>
   )
