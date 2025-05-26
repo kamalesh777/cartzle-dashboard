cbf765367e7e1770b6104d17c8e65ed2d2b8f9e9
kamalesh777 <ksmaity777>
2025-05-26 21:11:51 +0530

    Dev - new menus added

--- app/demo-1/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import DemoOne from '@/modules/DemoOne'
-
-const DemoPage = (): JSX.Element => {
-  return <DemoOne />
-}
-
-export default DemoPage
--- app/demo/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const DemoPage = (): JSX.Element => {
-  return <div>DemoPage</div>
-}
-
-export default DemoPage
