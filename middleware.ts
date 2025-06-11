bee57cd8c081e3892889b3049017669fd9db4707
kamalesh777 <ksmaity777>
2025-06-11 22:23:30 +0530

    Dev - middleware placed on root to execute it all route change

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { cookies } from 'next/headers'
+import { type NextRequest, NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function middleware(request: NextRequest) {
+  const cookieStore = cookies()
+  const accessToken = cookieStore.get('accessToken')
+  return NextResponse.rewrite(request.url)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server'
-
-// This function can be marked `async` if using `await` inside
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export function middleware(request: NextRequest) {
-  console.log("===request", request)
-  return NextResponse.redirect(new URL('/home', request.url))
-}
