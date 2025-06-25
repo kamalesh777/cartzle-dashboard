336888711d4684ca0944917bedd3a0a8614c679e
kamalesh777 <ksmaity777>
2025-06-25 22:13:22 +0530

    Dev - disble dependency checking

--- src/modules/settings/product-settings/category/list/index.tsx
+++ src/modules/settings/product-settings/category/list/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react'
 
 import type { CategoryList } from '../../types'
--- src/modules/settings/product-settings/unit-types/list/index.tsx
+++ src/modules/settings/product-settings/unit-types/list/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react'
 
 import { Row, type MenuProps } from 'antd'
