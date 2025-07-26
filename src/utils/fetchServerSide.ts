e9c16ebcf0898a25a43b03e29da937ed12ca30c3
kamalesh777 <ksmaity777>
2025-07-26 23:56:41 +0530

    Dev - serverside added for get, post etc

--- src/utils/fetchServerSide.ts
+++ src/utils/fetchServerSide.ts
@@ -2,28 +2,32 @@ import { cookies } from 'next/headers'
 
 import { API_BASE_URL } from '@/constants/ApiConstant'
 
-export const fetchServerSide = async (endpoint: string): Promise<any> => {
+export const requestServerSide = async (endpoint: string, method: string, body?: any): Promise<any> => {
   const Cookies = await cookies()
 
   // use params if present
-  const params = new URLSearchParams()
+  const searchQuery = new URLSearchParams()
   if (endpoint.includes('?')) {
     const paramPairs = endpoint.split('?')[1].split('&')
 
     paramPairs.forEach(pair => {
       const [key, value] = pair.split('=')
-      params.set(key, value)
+      searchQuery.set(key, value)
     })
   }
 
-  // check if params are present
-  const paramsString = params.toString() ? `?${params.toString()}` : ''
+  // check if searchQuery are present
+  const paramsString = searchQuery.toString() ? `?${searchQuery.toString()}` : ''
 
   const response = await fetch(`${API_BASE_URL}${endpoint}${paramsString}`, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${Cookies.get('accessToken')?.value}`,
     },
+    method,
+    body,
   })
   return response.json()
 }
+
+export const fetchServerSide = requestServerSide
