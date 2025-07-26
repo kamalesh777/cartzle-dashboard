9537d6a47fe9d7c67cfa174f4d9e022a9b3ecb0f
kamalesh777 <ksmaity777>
2025-07-26 16:25:25 +0530

    Dev - next config error fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const url = new URL(process.env.NEXT_PUBLIC_API_SERVICE)
-
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
@@ -13,15 +11,6 @@ const nextConfig = {
         permanent: true,
       },
     ]
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: url.protocol.replace(':', ''),
-        hostname: url.hostname,
-        port: url.port,
-      },
-    ],
   },
 }
 module.exports = nextConfig
