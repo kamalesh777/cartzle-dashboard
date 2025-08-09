d14fa006028b1e28ba51a9d0d035cf582680cbe9
kamalesh777 <ksmaity777>
2025-08-09 18:07:12 +0530

    Dev - fixed build error

--- src/modules/products/manage/Components/PriceCard.tsx
+++ src/modules/products/manage/Components/PriceCard.tsx
@@ -1,73 +1,76 @@
+import React, { useEffect } from 'react'
+
+import { Form, Row } from 'antd'
+
+// eslint-disable-next-line no-duplicate-imports
+import type { FormInstance } from 'antd'
+
 import { CardWrapper, ColWrapper, FormItemWrapper, InputNumberWrapper } from '@/components/Wrapper'
 import { COMMON_ROW_GUTTER, EMPTY_PLACEHOLDER, requiredFieldRules } from '@/constants/AppConstant'
 import { getCurrency, getProfitDiscount } from '@/utils/currency'
-import { Form, Row } from 'antd'
-import React, { useEffect } from 'react'
-import { FormInstance } from 'antd'
 
 interface PropTypes {
   form: FormInstance
 }
 
-const PriceCard = ({ form }: PropTypes) => {
-
+const PriceCard = ({ form }: PropTypes): JSX.Element => {
   const costPrice = Form.useWatch('costPrice', form)
   const salePrice = Form.useWatch('salePrice', form)
   const discount = Form.useWatch('discount', form)
 
   // Set profit and margin on cost price and sale price change
-    useEffect(() => {
-      if (salePrice > costPrice) {
-        form.setFieldsValue({
-          profit: getProfitDiscount(costPrice, salePrice, discount, 'profit'),
-        })
-      } else {
-        form.setFieldsValue({
-          profit: EMPTY_PLACEHOLDER,
-          discount: 0,
-        })
-      }
-    }, [costPrice, salePrice, discount, form])
+  useEffect(() => {
+    if (salePrice > costPrice) {
+      form.setFieldsValue({
+        profit: getProfitDiscount(costPrice, salePrice, discount, 'profit'),
+      })
+    } else {
+      form.setFieldsValue({
+        profit: EMPTY_PLACEHOLDER,
+        discount: 0,
+      })
+    }
+  }, [costPrice, salePrice, discount, form])
   return (
     <CardWrapper title={'Pricing'} className="mb-3" bottomBorderNone>
-          <Row gutter={COMMON_ROW_GUTTER}>
-            <ColWrapper md={12}>
-              <FormItemWrapper name="costPrice" label="Cost Price" rules={requiredFieldRules}>
-                <InputNumberWrapper prefix={getCurrency()} />
-              </FormItemWrapper>
-            </ColWrapper>
-            <ColWrapper md={12}>
-              <FormItemWrapper
-                name="salePrice"
-                label="Sale Price"
-                dependencies={['costPrice']}
-                rules={[
-                  ...requiredFieldRules,
-                  ({ getFieldValue }) => ({
-                    validator(_, value) {
-                      if (!value || value > getFieldValue('costPrice')) {
-                        return Promise.resolve()
-                      }
-                      return Promise.reject(new Error('Sale price < Cost price'))
-                    },
-                  }),
-                ]}
-              >
-                <InputNumberWrapper prefix={getCurrency()} />
-              </FormItemWrapper>
-            </ColWrapper>
-            <ColWrapper md={12}>
-              <FormItemWrapper name="discount" label="Discount" className="mb-2">
-                <InputNumberWrapper suffix={'%'} />
-              </FormItemWrapper>
-            </ColWrapper>
-            <ColWrapper md={12}>
-              <FormItemWrapper name="profit" label="Profit" className="mb-2">
-                <InputNumberWrapper readOnly prefix={getCurrency()} />
-              </FormItemWrapper>
-            </ColWrapper>
-          </Row>
-        </CardWrapper>
+      <Row gutter={COMMON_ROW_GUTTER}>
+        <ColWrapper md={12}>
+          <FormItemWrapper name="costPrice" label="Cost Price" rules={requiredFieldRules}>
+            <InputNumberWrapper prefix={getCurrency()} />
+          </FormItemWrapper>
+        </ColWrapper>
+        <ColWrapper md={12}>
+          <FormItemWrapper
+            name="salePrice"
+            label="Sale Price"
+            dependencies={['costPrice']}
+            rules={[
+              ...requiredFieldRules,
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || value > getFieldValue('costPrice')) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('Sale price < Cost price'))
+                },
+              }),
+            ]}
+          >
+            <InputNumberWrapper prefix={getCurrency()} />
+          </FormItemWrapper>
+        </ColWrapper>
+        <ColWrapper md={12}>
+          <FormItemWrapper name="discount" label="Discount" className="mb-2">
+            <InputNumberWrapper suffix={'%'} />
+          </FormItemWrapper>
+        </ColWrapper>
+        <ColWrapper md={12}>
+          <FormItemWrapper name="profit" label="Profit" className="mb-2">
+            <InputNumberWrapper readOnly prefix={getCurrency()} />
+          </FormItemWrapper>
+        </ColWrapper>
+      </Row>
+    </CardWrapper>
   )
 }
 
