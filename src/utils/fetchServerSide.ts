32ca85de768ab65515e3ea03f8a54e254a64ebe5
kamalesh777 <ksmaity777>
2025-09-19 22:56:24 +0530

    Dev - error handled on fetch server side props

--- src/utils/fetchServerSide.ts
+++ src/utils/fetchServerSide.ts
@@ -8,38 +8,45 @@ export const requestServerSide = async (
   method: Methods = 'get',
   body?: any,
 ): Promise<any> => {
-  const Cookies = await cookies()
+  try {
+    const Cookies = await cookies()
 
-  // use params if present
-  const searchQuery = new URLSearchParams()
-  if (endpoint.includes('?')) {
-    const paramPairs = endpoint.split('?')[1].split('&')
+    // use params if present
+    const searchQuery = new URLSearchParams()
+    if (endpoint.includes('?')) {
+      const paramPairs = endpoint.split('?')[1].split('&')
 
-    paramPairs.forEach(pair => {
-      const [key, value] = pair.split('=')
-      searchQuery.set(key, value)
+      paramPairs.forEach(pair => {
+        const [key, value] = pair.split('=')
+        searchQuery.set(key, value)
+      })
+    }
+
+    // check if searchQuery are present
+    const paramsString = searchQuery.toString() ? `?${searchQuery.toString()}` : ''
+
+    // const response = await fetch(`${API_BASE_URL}${endpoint}${paramsString}`, {
+    const response = await fetch(`${endpoint}${paramsString}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Cookies.get('accessToken')?.value}`,
+      },
+      method,
+      body,
     })
+
+    // convert response to json
+    const finalResponse = {
+      ...(await response.json()),
+      status: response.status,
+    }
+    return finalResponse
+  } catch (error) {
+    return {
+      error: 'Failed to fetch data',
+      status: 500,
+    }
   }
-
-  // check if searchQuery are present
-  const paramsString = searchQuery.toString() ? `?${searchQuery.toString()}` : ''
-
-  // const response = await fetch(`${API_BASE_URL}${endpoint}${paramsString}`, {
-  const response = await fetch(`${endpoint}${paramsString}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${Cookies.get('accessToken')?.value}`,
-    },
-    method,
-    body,
-  })
-
-  // convert response to json
-  const finalResponse = {
-    ...(await response.json()),
-    status: response.status,
-  }
-  return finalResponse
 }
 
 export const fetchServerSide = requestServerSide
