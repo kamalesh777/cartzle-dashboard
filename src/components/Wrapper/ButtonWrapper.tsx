c9b5c28ff898d61f12c22184273ee95afe3ac3c4
kamalesh777 <ksmaity777>
2025-09-20 08:41:50 +0530

    Dev - design is started for theme builder

--- src/components/Wrapper/ButtonWrapper.tsx
+++ src/components/Wrapper/ButtonWrapper.tsx
@@ -8,15 +8,26 @@ import type { ButtonProps } from 'antd'
 interface CustomButtonProps extends ButtonProps {
   label?: string
   tooltip?: string
+  noStyle?: boolean
 }
 
 const ButtonWrapper: React.FC<CustomButtonProps> = props => {
-  return props.tooltip ? (
-    <Tooltip title={props.tooltip}>
-      <Button {...props}>{props.children}</Button>
-    </Tooltip>
-  ) : (
-    <Button {...props}>{props.children}</Button>
+  const className = props?.className || ''
+  const additionalClassName = props.noStyle ? 'no-style' : ''
+  return (
+    <>
+      {props.tooltip ? (
+        <Tooltip title={props.tooltip}>
+          <Button className={className + additionalClassName} {...props}>
+            {props.children}
+          </Button>
+        </Tooltip>
+      ) : (
+        <Button className={className + additionalClassName} {...props}>
+          {props.children}
+        </Button>
+      )}
+    </>
   )
 }
 
--- src/modules/admin/theme-library/manage/ThemeBuilder.tsx
+++ src/modules/admin/theme-library/manage/ThemeBuilder.tsx
@@ -1,13 +1,14 @@
 'use client'
 import React from 'react'
 
-import { Collapse, Row, Tabs } from 'antd'
+import { Collapse, Form, Row, Tabs } from 'antd'
 
-import { PanelsTopLeft } from 'lucide-react'
+import { PanelsTopLeft, Save, X } from 'lucide-react'
 
-import { CardWrapper, ColWrapper, SpaceWrapper } from '@/components/Wrapper'
+import { ButtonWrapper, CardWrapper, ColWrapper, SelectWrapper, SpaceWrapper } from '@/components/Wrapper'
 
 // eslint-disable-next-line no-duplicate-imports
+import FormWrapper from '@/components/Wrapper/FormWrapper'
 import IconWrapper, { type IconProps } from '@/components/Wrapper/IconWrapper'
 
 import LayoutCardComp from '../components/LayoutCard'
@@ -15,53 +16,72 @@ import { tabsArray } from '../static/data'
 import { pagesArray } from '../static/layout-card'
 
 const ThemeBuilder = (): JSX.Element => {
+  const [form] = Form.useForm()
   return (
-    <Row>
-      {/* panel for config layout */}
-      <ColWrapper>
-        {pagesArray.map((obj, idx) => (
-          <Collapse
-            key={idx}
-            // collapsible="header"
-            className="mb-3"
-            expandIconPosition="end"
-            defaultActiveKey={['home']}
-            items={[
-              {
-                key: obj.id,
-                label: (
-                  <SpaceWrapper>
-                    <PanelsTopLeft className="lucide-icon-1-3" />
-                    <span className="fw-semibold">{obj.label}</span>
-                  </SpaceWrapper>
-                ),
-                classNames: {
-                  header: 'bg-white w-100 rounded-2',
+    <FormWrapper form={form} log>
+      <Row>
+        {/* panel for config layout */}
+        <ColWrapper>
+          {pagesArray.map((obj, idx) => (
+            <Collapse
+              key={idx}
+              // collapsible="header"
+              className="mb-2"
+              expandIconPosition="end"
+              defaultActiveKey={['home']}
+              items={[
+                {
+                  key: obj.id,
+                  label: (
+                    <SpaceWrapper>
+                      <PanelsTopLeft className="lucide-icon-1-3" />
+                      <span className="fw-semibold">{obj.label}</span>
+                    </SpaceWrapper>
+                  ),
+                  classNames: {
+                    header: 'bg-white w-100 rounded-2',
+                  },
+                  children: <LayoutCardComp />,
+                  extra: (
+                    <SpaceWrapper size={8}>
+                      <ButtonWrapper noStyle icon={<X className="error-color-on-hover" />} />
+                      <ButtonWrapper noStyle icon={<Save className="primary-color" />} />
+                    </SpaceWrapper>
+                  ),
                 },
-                children: <LayoutCardComp />,
-              },
-            ]}
-          />
-        ))}
-      </ColWrapper>
+              ]}
+            />
+          ))}
+        </ColWrapper>
 
-      {/* Page Builder */}
-      <ColWrapper span={24}>
-        <CardWrapper bottomBorderNone classNames={{ body: 'px-0' }}>
-          <Tabs
-            centered
-            className="builder-tabs"
-            defaultActiveKey="2"
-            items={tabsArray.map(obj => {
-              return {
-                ...obj,
-                icon: <IconWrapper name={obj.icon as IconProps['name']} />,
-              }
-            })}
-          />
-        </CardWrapper>
-      </ColWrapper>
-    </Row>
+        {/* Page Builder */}
+        <ColWrapper span={24}>
+          <CardWrapper bottomBorderNone>
+            <Tabs
+              centered
+              className="builder-tabs"
+              defaultActiveKey="2"
+              tabBarExtraContent={{
+                left: (
+                  <SelectWrapper
+                    placeholder="Select Page"
+                    placement="bottomLeft"
+                    options={pagesArray.map(obj => ({ label: obj.label, value: obj.id }))}
+                  />
+                ),
+                right: <ButtonWrapper type="primary">Save</ButtonWrapper>,
+              }}
+              items={tabsArray.map(obj => {
+                return {
+                  ...obj,
+                  icon: <IconWrapper name={obj.icon as IconProps['name']} />,
+                }
+              })}
+            />
+          </CardWrapper>
+        </ColWrapper>
+      </Row>
+    </FormWrapper>
   )
 }
 
